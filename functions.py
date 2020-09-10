531870ed7a13c89dc4a9337577a215467e04373e
JayWasTaken <jaymay8k@gmail.com>
2020-09-09 20:17:27 -0700

    initial

---
+++ functions.py
@@ -0,0 +1,28 @@
+def area(width, height):
+    result = width * height
+    return result
+    
+result = area(5, 6)
+print(result)
+result = area(2, 5)
+print(result)
+result = area(8, 2)
+print(result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(8, 2)
+print(result)
+result = subtract(1, 3)
+print(result)
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+result = add(3, 5)
+print(result)
+result = add(2, -8)
+print(result)
